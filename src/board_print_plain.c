eedbc729004c2f85a557a001cf8c30f9fc2bc85a
maksim <MaxChip322@yandex.ru>
2018-04-16 19:44:34 +0700

    add init and print board

--- src/board_print_plain.c
+++ src/board_print_plain.c
@@ -1,16 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void initBoard ( char Board[9][9]) {
+int o,p;
+int u;
+for (o=0; o<9; o++){
+    for(p=0; p<9; p++){
+    Board[o][p] = ' ';
+    }
+}
+for (o=1; o<9; o++){
+Board[1][o] = 'p';
+}
+for (o=0,u=8; o<8; o++,u--){
+Board[o][0] = u + '0';
+}
+Board[0][1] = 'r'; Board[0][2] = 'n'; Board[0][3] = 'b'; Board[0][4] = 'q';
+Board[0][5] = 'k'; Board[0][6] = 'b'; Board[0][7] = 'n'; Board[0][8] = 'r';
+for (o=1; o<9; o++){
+Board[6][o] = 'P';
+}
+Board[7][1] = 'R'; Board[7][2] = 'N'; Board[7][3] = 'B'; Board[7][4] = 'Q';
+Board[7][5] = 'K'; Board[7][6] = 'B'; Board[7][7] = 'N'; Board[7][8] = 'R';
+for (o=1,u=97; o<9;o++,u++){
+Board[8][o] = u;
+}
+}
 
-
-void printBoard(char &Board) {
-	int i,j;
-	
-	for(i=0;i<10;i++) {
-		printf("\n");
-		for(j=0;j<10;j++) {
-			printf("%2c",Board[i][j]);
-		}
+void printBoard ( char Board[9][9]) {
+    int o,p;
+    for (o=0; o<9; o++){
+	for (p=0; p<9; p++){
+	    printf("%c ",Board[o][p]);
 	}
+	printf("\n");
+    }
 }
 	
