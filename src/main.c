ac54113835ba91b1e4acf7ebfad8c1931bfbc903
maksim <MaxChip322@yandex.ru>
2018-04-16 20:04:54 +0700

    add header file

--- src/main.c
+++ src/main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "board_print_plain.c"
+#include "board_print_plain.h"
 
 
-int main () {
+int main (int argc, char *argv[]) {
 	
 	char Board[9][9];
 
